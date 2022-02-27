5b2f1a562c9b49865d034b6c066c09c06e329e04
quinteroqca <51741190+quinteroqca@users.noreply.github.com>
2022-02-27 21:39:50 +0100

    improving interface (#2)

--- netgen/heterogenousBlockSizes.py
+++ netgen/heterogenousBlockSizes.py
@@ -8,21 +8,24 @@ Created on Sun Feb 13 21:44:01 2022
 from numpy.random import rand
 from typing import List
 
-def heterogenousBlockSizes(B:int,N:int,x_min:int = None,alpha:float = 2.5) -> List[int]:
+from .mod_param import mod_param
+
+
+def heterogenousBlockSizes(B: int, N: int, min_block_size: int = 0, alpha: float = 2.5) -> List[int]:
     """
     This function will generate heterogenous block sizes, following a powerlaw distribution.
     As in Clauset A, et al. SIAM Rev., 51(4), 661–703 2009.
 
-    The transformation method is used to generate x samples from a powerlaw distribution. 
-    These samples will be the sizes of the blocks across each dimension, the functions is called 
-    for col and rows, separately. 
-    Two elements are needed: uniformly distributed random source r where 0 <= r < 1 and 
+    The transformation method is used to generate x samples from a powerlaw distribution.
+    These samples will be the sizes of the blocks across each dimension, the functions is called
+    for col and rows, separately.
+    Two elements are needed: uniformly distributed random source r where 0 <= r < 1 and
     the functional inverse of the cumulative density function (CDF) of the power-law distribution
-    
+
     The source r is simply given as a parameter to the CDF.
-    
+
     the inputs are:
-    
+
     N: int
         numnber of nodes to split into block
     B: number
@@ -30,23 +33,28 @@ def heterogenousBlockSizes(B:int,N:int,x_min:int = None,alpha:float = 2.5) -> Li
     x_min: int
         be the lower bound, i.e, minimum block size, default 10% of N
     alpha: float bounded in (x1,x2)
-        be the scaling parameter of the distribution, default 2.5    
+        be the scaling parameter of the distribution, default 2.5
     output:
-    
+
     colsizes: list of ints
         a list containing B random numbers determining the size of each block
     """
-    if x_min is None:
-        x_min=(N*0.1)
+    if min_block_size == 0:
+        min_block_size = int(N * 0.1)
+    if alpha == 0:
+        if N % B != 0:
+            raise ValueError(f"The number of nodes must be divisible by B. {N} % {B} = {N%B}")
+        else:
+            return [mod_param(N, N, B)[0]] * B
     if 3 <= alpha or alpha < 1:
         raise ValueError("alpha must be between (1,3)")
-    clsum=1
-    maxn=N
-    while (clsum!=N) or (maxn>=N/2):
+    clsum = 1
+    maxn = N
+    while (clsum != N) or (maxn >= N / 2):
         r = rand(B)
-        x_smp = x_min * (1 - r) ** (-1 / (alpha - 1))
+        x_smp = min_block_size * (1 - r) ** (-1 / (alpha - 1))
         colsizes = x_smp.round(0).astype(int).tolist()
-        clsum=sum(colsizes)
-        maxn=max(colsizes)
+        clsum = sum(colsizes)
+        maxn = max(colsizes)
     colsizes.sort(reverse=True)
     return colsizes
